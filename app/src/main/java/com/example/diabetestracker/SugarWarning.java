c18a7936446c34b11c4716ed54ab2f744f18dc71
Khadijaaimen <58134039+Khadijaaimen@users.noreply.github.com>
2020-07-27 19:39:59 +0500

    Statistics and Warning

--- app/src/main/java/com/example/diabetestracker/SugarWarning.java
+++ app/src/main/java/com/example/diabetestracker/SugarWarning.java
@@ -2,6 +2,7 @@ package com.example.diabetestracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SugarWarning extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class SugarWarning extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sugar_warning);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, SugarLog.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/diabetestracker/TargetLevels.java
+++ app/src/main/java/com/example/diabetestracker/TargetLevels.java
@@ -2,6 +2,7 @@ package com.example.diabetestracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class TargetLevels extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class TargetLevels extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, SugarLog.class));
+        finish();
     }
 }
